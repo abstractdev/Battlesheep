60d76f1946e010a76e40b3bf034fa68ec5f2fb72
fadcny <fadcny@gmail.com>
2022-03-30 18:22:03 -0400

    cover edge cases

--- battleship.js
+++ battleship.js
@@ -38,35 +38,53 @@ export const Gameboard = (boardName) => {
   const carrier = Ship("Carrier", 5);
 
   function placeShip(xStart, yStart, ship, horizontal, vertical) {
-    //if ship exceeds grid with starting position, adjust starting value//
-    const xStartAdjusted = 10 - ship.length;
-    const yStartAdjusted = 10 - ship.length;
+    //prevent placing multiples of the same ship
+    const flatBoard = board.flat()
+    if ((flatBoard.some((e) => {
+      return (e === ship.ship[0] || e === `${ship.ship[0]}v`)
+    }))) {
+      return
+    }
 
     if (vertical) {
-      if (xStart + ship.length > 9) {
-        placeShipVertically(xStartAdjusted, yStart, ship)
-    }else {
-      placeShipVertically(xStart, yStart, ship)
+       //prevent invalid placement
+       if (xStart + ship.length > 10) {
+        return
       }
+      //default placement
+      placeShipVertically(xStart, yStart, ship)
     }
 
     if (horizontal) {
-      if (xStart + ship.length > 9) {
-        placeShipHorizontally(yStartAdjusted, xStart, ship)
-    }else {
-      placeShipHorizontally(yStart, xStart, ship)
-      }
+    //prevent invalid placement
+       if (yStart + ship.length > 10) {
+         return
+        }
+        //default placement
+      placeShipHorizontally(xStart, yStart, ship)
     }
   }
 
-  function placeShipVertically(x, yStart, ship) {
+  function placeShipVertically(xStart, yStart, ship) {
+    //prevent placing ships in overlapping positions
+    for (let z = 0; z < ship.length; z++) {
+      if (board[z + xStart][yStart] !== 0) {
+        return
+      }
+    }
     for (let i = 0; i < ship.length; i++) {
-        board[i + x].splice(yStart, 1, ship.ship[i]);
+        board[i + xStart].splice(yStart, 1, `${ship.ship[i]}v`);
     }
   }
-  function placeShipHorizontally(y, xStart, ship) {
+  function placeShipHorizontally(xStart, yStart, ship) {
+    //prevent placing ships in overlapping positions
+    for (let z = 0; z < ship.length; z++) {
+      if (board[xStart][z + yStart] !== 0) {
+        return
+      }
+    }
     for (let i = 0; i < ship.length; i++) {
-        board[xStart].splice((i + y), 1, ship.ship[i]);
+        board[xStart].splice((i + yStart), 1, ship.ship[i]);
     }
   }
 
@@ -75,60 +93,58 @@ export const Gameboard = (boardName) => {
       case 0:
         board[x][y] = 1
         break;
-      // case 1:
-      //   break;
-      case "Destroyer0":
+      case "Destroyer0" || "Destroyer0v":
         destroyer.hit(0)
         updateBoardHitState(board, x, y)
-      case "Destroyer1":
+      case "Destroyer1" || "Destroyer1v":
         updateBoardHitState(board, x, y)
         break;
-      case "Submarine0":
+      case "Submarine0" ||  "Submarine0v":
           submarine.hit(0)
           updateBoardHitState(board, x, y) 
-      case "Submarine1":
+      case "Submarine1" ||  "Submarine1v":
           submarine.hit(1)
           updateBoardHitState(board, x, y) 
-      case "Submarine2":
+      case "Submarine2" ||  "Submarine2v":
           submarine.hit(2)
           updateBoardHitState(board, x, y) 
         break;
-      case "Cruiser0":
+      case "Cruiser0" ||  "Cruiser0v":
           cruiser.hit(0)
           updateBoardHitState(board, x, y) 
-      case "Cruiser1":
+      case "Cruiser1" ||  "Cruiser1v":
           cruiser.hit(1)
           updateBoardHitState(board, x, y) 
-      case "Cruiser2":
+      case "Cruiser2" ||  "Cruiser2v":
           cruiser.hit(2)
           updateBoardHitState(board, x, y)
         break;
-      case "Battleship0":
+      case "Battleship0" ||  "Battleship0v":
           battleship.hit(0)
           updateBoardHitState(board, x, y) 
-      case "Battleship1":
+      case "Battleship1" ||  "Battleship1v":
           battleship.hit(1)
           updateBoardHitState(board, x, y) 
-      case "Battleship2":
+      case "Battleship2" ||  "Battleship2v":
           battleship.hit(2)
           updateBoardHitState(board, x, y) 
-      case "Battleship3":
+      case "Battleship3" ||  "Battleship3v":
           battleship.hit(3)
           updateBoardHitState(board, x, y) 
         break;
-      case "Carrier0":
+      case "Carrier0" ||  "Carrier0v":
           carrier.hit(0)
           updateBoardHitState(board, x, y) 
-      case "Carrier1":
+      case "Carrier1" ||  "Carrier1v":
           carrier.hit(1)
           updateBoardHitState(board, x, y) 
-      case "Carrier2":
+      case "Carrier2" ||  "Carrier2v":
           carrier.hit(2)
           updateBoardHitState(board, x, y) 
-      case "Carrier3":
+      case "Carrier3" ||  "Carrier3v":
           carrier.hit(3)
           updateBoardHitState(board, x, y) 
-      case "Carrier4":
+      case "Carrier4" ||  "Carrier4v":
           carrier.hit(4)
           updateBoardHitState(board, x, y)
         break;
@@ -138,15 +154,18 @@ export const Gameboard = (boardName) => {
   function updateBoardHitState(board, x, y) {
     board[x][y] = 'hit';
   }
-  function setEndGameCondition (hitCounter) {
-    if (hitCounter === 17) {
-      board[9][9] = 'GAME IS OVER'
-    }
-  }
-  function convertInputToAttack(e) {
+  function convertInputToCoordinates(e) {
     const displayId = e.target.dataset.id;
-    const firstIndex = displayId.charAt(8);
-    const secondIndex = displayId.charAt(9);
+    let firstIndex;
+    let secondIndex;
+    if (displayId.charAt(0) === 'c') {
+      firstIndex = displayId.charAt(8);
+      secondIndex = displayId.charAt(9);
+      console.log(firstIndex);
+    }else if (displayId.charAt(0) === 'p') {
+      firstIndex = displayId.charAt(6);
+      secondIndex = displayId.charAt(7);
+    }
     const coordinates = [(parseInt(firstIndex)),(parseInt(secondIndex))]
     return coordinates
   }
@@ -165,6 +184,15 @@ export const Gameboard = (boardName) => {
     allCoordinatesFlat.splice(randomIndex, 1)
     return savedRandomCoordinates
   }
+
+  function placeComputerShips(computerGameboard) {
+    computerGameboard.placeShip(0, 0, computerGameboard.destroyer, true, false)
+    computerGameboard.placeShip(1, 0, computerGameboard.submarine, true, false)
+    computerGameboard.placeShip(2, 0, computerGameboard.cruiser, true, false)
+    computerGameboard.placeShip(3, 0, computerGameboard.battleship, true, false)
+    computerGameboard.placeShip(4, 0, computerGameboard.carrier, true, false)
+  }
+
   return {
     boardName,
     board,
@@ -175,8 +203,9 @@ export const Gameboard = (boardName) => {
     carrier,
     placeShip,
     receiveAttack,
-    convertInputToAttack,
-    getRandomCoordinates
+    convertInputToCoordinates,
+    getRandomCoordinates,
+    placeComputerShips
   };
 };
 
