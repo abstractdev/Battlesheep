69484caafffce85336e123c3a731b67162b8e316
fadcny <fadcny@gmail.com>
2022-03-31 10:10:28 -0400

    winner modal. computer ship placement

--- game.js
+++ game.js
@@ -3,11 +3,13 @@ import { Player } from "./battleship.js";
 import { Gameboard } from "./battleship.js";
 import { Display } from "./display.js";
 
-const Game = (() => {
+export const Game = (() => {
+  
   const playerGameboard = Gameboard("player");
   const computerGameboard = Gameboard("computer");
   const player = Player("Player", "player", true);
   const computer = Player("Computer", "computer", false);
+  computerGameboard.placeComputerShips(computerGameboard);
   let playerHitCounter = 0
   let computerHitCounter = 0
   
@@ -23,25 +25,15 @@ const Game = (() => {
     document.querySelector(".computer-gameboard-container")
   );
   //place ships
-  playerGameboard.placeShip(0, 0, playerGameboard.destroyer, true, false)
-  playerGameboard.placeShip(1, 0, playerGameboard.submarine, true, false)
-  playerGameboard.placeShip(2, 0, playerGameboard.cruiser, true, false)
-  playerGameboard.placeShip(3, 0, playerGameboard.battleship, true, false)
-  playerGameboard.placeShip(4, 0, playerGameboard.carrier, true, false)
-  computerGameboard.placeShip(0, 0, computerGameboard.destroyer, true, false)
-  computerGameboard.placeShip(1, 0, computerGameboard.submarine, true, false)
-  computerGameboard.placeShip(2, 0, computerGameboard.cruiser, true, false)
-  computerGameboard.placeShip(3, 0, computerGameboard.battleship, true, false)
-  computerGameboard.placeShip(4, 0, computerGameboard.carrier, true, false)
+  Display.initShipPlacement();
   //1 turn
     document.querySelector('.computer-gameboard-container').addEventListener('click', (e) => {
       if (e.target.className === 'cell') {
       //record user attack on board array
-        const coordinates = computerGameboard.convertInputToAttack(e);
+        const coordinates = computerGameboard.convertInputToCoordinates(e);
         computerGameboard.receiveAttack(coordinates[0], coordinates[1])
         if (computerGameboard.board[coordinates[0]][coordinates[1]] === 'hit') {
           computerHitCounter++
-          console.log(computerHitCounter);
         }
       }
       //display user attack
@@ -55,7 +47,6 @@ const Game = (() => {
         playerGameboard.receiveAttack(randomCoordinates[0], randomCoordinates[1])
         if (playerGameboard.board[randomCoordinates[0]][randomCoordinates[1]] === 'hit') {
           playerHitCounter++
-          console.log(playerHitCounter);
         }
       //display computer move
         Display.renderBoard(
@@ -64,9 +55,16 @@ const Game = (() => {
           document.querySelector(".player-gameboard-container")
           );
           
-          if (computerHitCounter === 17 || playerHitCounter === 17) {
-            alert('Game Over')
+          if (computerHitCounter === 17) {
+            Display.displayWinnerModal(`${player.playerName} wins!`)
+            document.querySelector('body').style.pointerEvents = 'none';
+          }else if (playerHitCounter === 17){
+            Display.displayWinnerModal(`${computer.playerName} wins!`)
+            document.querySelector('body').style.pointerEvents = 'none';
           }
     })
+    return {
+      playerGameboard
+    }
 })();
 
